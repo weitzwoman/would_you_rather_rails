18fea79ee9cad8d0faaa6e9ab37037e7e4173643
Tyler Ferrin and Tracie Weitzman <tyler@>
2017-02-20 15:41:18 -0800

    add fake news to our site

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,9 +2,22 @@ class AnswersController < ApplicationController
   def show
   end
   def upvote
+
     @answer = Answer.find(params[:id])
-    @answer.upvote_by current_user
-    redirect_to :back
+    @question = @answer.question
+    @question.answers.each do |a|
+      a.get_upvotes.each do |vote|
+        if current_user.id == vote.voter_id
+          redirect_to questions_path
+
+          flash[:alert] = " YOU CANT VOTE TWICE! FAKE NEWS!"
+          return
+        else
+          @answer.upvote_by current_user
+          redirect_to questions_path
+        end
+      end
+    end
   end
 
 end
